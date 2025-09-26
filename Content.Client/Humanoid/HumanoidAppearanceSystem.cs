970eb594c9900a284c092337a084c79c4f487c13
Kurycovwolff <joshuazllynx@gmail.com>
2025-09-25 16:11:27 -0900

    Uh /// everywher

--- Content.Client/Humanoid/HumanoidAppearanceSystem.cs
+++ Content.Client/Humanoid/HumanoidAppearanceSystem.cs
@@ -22,7 +22,7 @@ using System.Numerics;
 using Content.Client.DisplacementMap;
 using Content.Shared.CCVar;
 using Content.Shared.Humanoid;
-using Content.Shared.Humanoid.Markings;
+//using Content.Shared.Humanoid.Markings;
 using Content.Shared.Humanoid.Prototypes;
 using Content.Shared.Inventory;
 using Content.Shared.Preferences;
@@ -368,11 +368,11 @@ public sealed class HumanoidAppearanceSystem : SharedHumanoidAppearanceSystem
 
 
             // impstation edit begin - check if there's a shader defined in the markingPrototype's shader datafield, and if there is...
-			if (markingPrototype.Shader != null)
-			{
+			//if (markingPrototype.Shader != null)
+			//{
 			// use spriteComponent's layersetshader function to set the layer's shader to that which is specified.
-				sprite.LayerSetShader(layerId, markingPrototype.Shader);
-			}
+			//	sprite.LayerSetShader(layerId, markingPrototype.Shader);
+			//}
 			// impstation edit end
 
             _sprite.LayerSetVisible((entity.Owner, sprite), layerId, visible);
