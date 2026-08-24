88fc00b348b93e9b75182ecac995365bb2998952
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2026-08-23 22:14:53 -0400

    isawmill fix

--- Content.Server/_BRatbite/PermaBrig/PermaBrigManager.cs
+++ Content.Server/_BRatbite/PermaBrig/PermaBrigManager.cs
@@ -27,6 +27,8 @@ namespace Content.Server._BRatbite.PermaBrig
         [Dependency] private readonly IEntityManager _entManager = default!;
         [Dependency] private readonly IAdminLogManager _adminLogger = default!;
 
+        private ISawmill _sawmill = default!;
+
         private readonly List<Task> _pendingSaveTasks = new();
 
         private ProtoId<RadioChannelPrototype> SecurityChannel = "Security";
@@ -36,10 +38,10 @@ namespace Content.Server._BRatbite.PermaBrig
             _task.BlockWaitOnTask(Task.WhenAll(_pendingSaveTasks));
         }
 
-        private readonly ISawmill _sawmill = Logger.GetSawmill("server_permabrig");
 
         public void Initialize()
         {
+            _sawmill = Logger.GetSawmill("server_permabrig");
         }
 
         /// <summary>
